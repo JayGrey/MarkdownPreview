8ded6e73d7815a27a3ca572ff1944192193c2342
Kulishenko Igor <KulishenkoIV@oschadbank.ua>
2019-10-29 11:46:28 +0200

    change help component

--- src/components/Help.tsx
+++ src/components/Help.tsx
@@ -4,62 +4,103 @@ import * as React from "react";
 export const Help = (): JSX.Element => {
   return (
     <div className="col">
-      <h3 className="mb-4">markdown help</h3>
+      <h3 className="mb-4">markdown help:</h3>
 
       <div className="row">
         <div className="col">
+          <h4>Headers</h4>
           <pre>
             <code>
-              # This is an &lt;h1&gt; tag 
-              
-              ## This is an &lt;h2&gt; tag 
+              # This is an &lt;h1&gt; tag
+              <br />
+              ## This is an &lt;h2&gt; tag
+              <br />
               ###### This is an &lt;h6&gt; tag
+              <br />
             </code>
           </pre>
         </div>
         <div className="col">
           <h4>Emphasys</h4>
-          <p>*This text will be italic*</p>
-          <p>_This will also be italic_</p>
-          <p>**This text will be bold**</p>
-          <p>__This will also be bold__</p>
-          <p>_You **can** combine them_</p>
+          <pre>
+            <code>
+              *This text will be italic*
+              <br />
+              _This will also be italic_
+              <br />
+              **This text will be bold**
+              <br />
+              __This will also be bold__
+              <br />
+              _You **can** combine them_
+              <br />
+            </code>
+          </pre>
         </div>
         <div className="col">
           <h4>Unordered lists</h4>
-          <p>* Item 1</p>
-          <p>* Item 2</p>
-          <p>&nbsp;&nbsp;* Item 2a</p>
-          <p>&nbsp;&nbsp;* Item 2b</p>
+          <pre>
+            <code>
+              * Item 1<br />
+              * Item 2<br />
+              &nbsp;&nbsp;* Item 2a
+              <br />
+              &nbsp;&nbsp;* Item 2b
+              <br />
+            </code>
+          </pre>
         </div>
         <div className="col">
           <h4>Ordered lists</h4>
-          <p>1. Item 1</p>
-          <p>1. Item 2</p>
-          <p>1. Item 3</p>
-          <p>&nbsp;&nbsp;1. Item 3a</p>
-          <p>&nbsp;&nbsp;1. Item 3b</p>
+          <pre>
+            <code>
+              1. Item 1<br />
+              1. Item 2<br />
+              1. Item 3<br />
+              &nbsp;&nbsp;1. Item 3a
+              <br />
+              &nbsp;&nbsp;1. Item 3b
+              <br />
+            </code>
+          </pre>
         </div>
-        <div className="col">
+        
+      </div>
+      <div className="row">
+      <div className="col">
           <h4>Images</h4>
-          <p>![GitHub Logo](/images/logo.png)</p>
-          <p>Format: ![Alt Text](url)</p>
+          <pre>
+            <code>
+              ![GitHub Logo](/images/logo.png)
+              <br />
+              Format: ![Alt Text](url)
+              <br />
+            </code>
+          </pre>
         </div>
         <div className="col">
           <h4>Links</h4>
-          <p>http://github.com - automatic!</p>
-          <p>[GitHub](http://github.com)</p>
+          <pre>
+            <code>
+              http://github.com - automatic!<br />
+              [GitHub](http://github.com)<br />
+            </code>
+          </pre>
         </div>
         <div className="col">
           <h4>Blockquotes</h4>
-          <p>As Kanye West said:</p>
-          <p>&gt; We&apos;re living the future so</p>
-          <p>&gt; the present is our past.</p>
+          <pre><code>
+          As Kanye West said:<br />
+          &gt; We&apos;re living the future so<br />
+          &gt; the present is our past.<br />
+          </code></pre>
         </div>
         <div className="col">
           <h4>Inline code</h4>
-          <p>I think you should use an</p>
-          <p>`&lt;addr&gt;` element here instead.</p>
+          <pre><code>
+          I think you should use an<br />
+          `&lt;addr&gt;` element here instead.<br />
+          </code></pre>
         </div>
       </div>
     </div>
